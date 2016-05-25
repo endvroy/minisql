49bad58782e78c95f628442e21ff30c99e08e8e1
Roy <endvroy@gmail.com>
2016-05-25 21:45:47 +0800

    Block refactored to state machine

--- buffer_manager.py
+++ buffer_manager.py
@@ -1,51 +1,88 @@
-class FileBlockView:
-    def __init__(self, size, file=None, block_index=0):
-        self.dirty = False
-        self.pinned = False
+from datetime import datetime
+import heapq
+
+
+class FreeBlock:
+    state = 'free'
+
+    def __init__(self, size):
         self.size = size
         self._memory = bytearray(size)
+
+    def read(self):
+        raise RuntimeError('Trying to read a free block')
+
+    def write(self, data):
+        raise RuntimeError('Trying to write into a free block')
+
+    def flush(self):
+        raise RuntimeError('Trying to flush a free block')
+
+    def load(self, file, block_index):
         self.file = file
         self.block_index = block_index
-        if file is None:
-            self.free = True
-        else:
-            self.free = False
+        self.file.seek(self.size * block_index)
+        self.effective_bytes = self.file.readinto(self._memory)
+        self.dirty = False
+        self.__class__ = OccupiedBlock
 
-    @property
-    def memory(self):
-        return self._memory
-
-    @memory.setter
-    def memory(self, data):
-        self._memory = data
-        self.dirty = True
+    def release(self):
+        raise RuntimeError('Trying to release a free block')
 
     def pin(self):
-        if self.free:
-            raise RuntimeError('Trying to pin a free block')
-        else:
-            self.pinned = True
+        raise RuntimeError('Tring to pin a free block')
 
     def unpin(self):
-        if self.free:
-            raise RuntimeError('Trying to unpin a free block')
-        if self.pinned:
-            self.pinned = False
-        else:
-            raise RuntimeError('Trying to unpin an already unpinned block')
+        raise RuntimeError('Tring to unpin a free block')
+
+
+class OccupiedBlockBase:
+    def read(self):
+        self.last_accessed_time = datetime.now()
+        return self._memory[:self.effective_bytes]
+
+    def write(self, data):
+        self._memory[:self.effective_bytes] = data
+        self.dirty = True
+        self.last_accessed_time = datetime.now()
 
     def flush(self):
         if self.dirty:
             self.file.seek(self.block_index * self.size)
-            self.file.write(self.memory)
+            self.file.write(self._memory[:self.effective_bytes])
+            self.file.flush()
             self.dirty = False
+        self.last_accessed_time = datetime.now()
+
+    def load(self, file, block_index):
+        raise RuntimeError('Trying to load into an occupied block')
+
+
+class OccupiedBlock(OccupiedBlockBase):
+    state = 'occupied'
 
     def release(self):
-        if self.pinned:
-            raise RuntimeError('Trying to release a pinned block')
-        else:
-            self.flush()
-            self.free = True
+        self.flush()
+        self.__class__ = FreeBlock
+
+    def pin(self):
+        self.__class__ = PinnedBlock
+
+    def unpin(self):
+        raise RuntimeError('Trying to unpin an already unpinned block')
+
+
+class PinnedBlock(OccupiedBlockBase):
+    state = 'pinned'
+
+    def release(self):
+        raise RuntimeError('Trying to release a pinned block')
+
+    def pin(self):
+        raise RuntimeError('Trying to pin an already pinned block')
+
+    def unpin(self):
+        self.__class__ = OccupiedBlock
 
 
 class BufferManager:
@@ -53,7 +90,7 @@ class BufferManager:
     total_blocks = 1024
 
     def __init__(self):
-        self.blocks = [FileBlockView(self.block_size) for i in range(self.total_blocks)]
+        self.blocks = [FreeBlock(self.block_size) for i in range(self.total_blocks)]
 
     def _find_free_block(self):
         pass
