a2a29e24fead6b0647a4e3680d3c216bbb3098b2
CJC <cjc0722hz@gmail.com>
2016-06-02 17:01:49 +0800

    record manager is almost finished. Some exception handling code can be added later

--- record_manager.py
+++ record_manager.py
@@ -15,23 +15,25 @@ class Record:
         # Each record in file has 2 extra info: next's block_off and next's record_off
         self.record_struct = Struct(real_format)
         self.rec_per_blk = math.floor(BufferManager.block_size / self.record_struct.size)
-        self.first_free_rec, self.rec_amount = self.parse_header()
+        self.first_free_rec, self.rec_amount = self._parse_header()
         self.first_free_blk = math.floor(self.first_free_rec / self.rec_per_blk)
 
     def insert(self, attributes):
-        # Insert will fail when append to a full block, but this should be handled
-        # by catalog manager when it provides the block offset and record offset.
-        record_info = attributes + (-1,)  # a negative, means this is a effective record
+        """
+        1. Insert will fail when append to a full block, but this should be handled
+        2. by catalog manager when it provides the block offset and record offset.
+        """
+        record_info = attributes + (-1,)  # a negative, means this is a real record
         if self.first_free_rec >= 0:
             # There are space in free list
             block = self.buffer_manager.get_file_block(self.filename, self.first_free_blk)
             with pin(block):
                 data = block.read()
-                records = self.parse_block_data(data, self.first_free_blk)
+                records = self._parse_block_data(data, self.first_free_blk)
                 next_free_rec = records[self.first_free_rec][-1]
                 local_offset = self.first_free_rec - self.first_free_blk * self.rec_per_blk
                 records[local_offset] = record_info
-                new_data = self.generate_new_data(records, self.first_free_blk)
+                new_data = self._generate_new_data(records, self.first_free_blk)
                 block.write(new_data)
             position = self.first_free_rec
             self.first_free_rec = next_free_rec
@@ -41,15 +43,55 @@ class Record:
             block = self.buffer_manager.get_file_block(self.filename, block_offset)
             with pin(block):
                 data = block.read()
-                records = self.parse_block_data(data, block_offset)
+                records = self._parse_block_data(data, block_offset)
                 records.append(record_info)
-                new_data = self.generate_new_data(records, block_offset)
+                new_data = self._generate_new_data(records, block_offset)
                 block.write(new_data)
             position = self.rec_amount
-        self.update_header()
+        self._update_header()
         return position
 
-    def generate_new_data(self, records, blk_offset):
+    def remove(self, record_offset):
+        """Remove the record at specified position and update the freelist"""
+        block_offset = math.floor(record_offset / self.rec_per_blk)
+        local_offset = record_offset - block_offset * self.rec_per_blk
+        block = self.buffer_manager.get_file_block(self.filename, block_offset)
+        with pin(block):
+            data = block.read()
+            records = self._parse_block_data(data, block_offset)
+            records[local_offset][-1] = self.first_free_rec  # A positive number, putting this position into free list
+            self.first_free_rec = record_offset  # update the head of free list
+            new_data = self._generate_new_data(records, block_offset)
+            block.write(new_data)
+        self._update_header()
+
+    def modify(self, attributes, record_offset):
+        """
+          1. Assume that the provided record_offset must point to a real record
+          2. During update, file header keeps invariant.
+         """
+        block_offset = math.floor(record_offset / self.rec_per_blk)
+        local_offset = record_offset - block_offset * self.rec_per_blk
+        block = self.buffer_manager.get_file_block(self.filename, block_offset)
+        record_info = attributes + (-1,)  # Updated record must be real
+        with pin(block):
+            data = block.read()
+            records = self._parse_block_data(data, block_offset)
+            records[local_offset] = record_info
+            new_data = self._generate_new_data(records, block_offset)
+            block.write()
+
+    def read(self, record_offset):
+        """ Return the record at the corresponding position """
+        block_offset = math.floor(record_offset / self.rec_per_blk)
+        local_offset = record_offset - block_offset * self.rec_per_blk
+        block = self.buffer_manager.get_file_block(self.filename, block_offset)
+        with pin(block):
+            data = block.read()
+            records = self._parse_block_data(data, block_offset)
+        return tuple(records[local_offset][:-1])
+
+    def _generate_new_data(self, records, blk_offset):
         if blk_offset is 0:
             data = bytearray(self.header_struct.size)
         else:
@@ -58,9 +100,9 @@ class Record:
             data += self.record_struct.pack(*r)
         return data
 
-    def parse_block_data(self, data, blk_offset):
+    def _parse_block_data(self, data, blk_offset):
         upper_bound = len(data)
-        if (upper_bound-self.header_struct.size) % self.record_struct.size != 0:
+        if (upper_bound - self.header_struct.size) % self.record_struct.size != 0:
             upper_bound -= self.record_struct.size
         if blk_offset is 0:  # is the first block, need to consider the header
             lower_bound = self.header_struct.size
@@ -70,20 +112,7 @@ class Record:
                    for offset in range(lower_bound, upper_bound, self.record_struct.size)]
         return records
 
-    def remove(self, record_offset):
-        block_offset = math.floor(record_offset / self.rec_per_blk)
-        local_offset = record_offset - block_offset * self.rec_per_blk
-        block = self.buffer_manager.get_file_block(self.filename, block_offset)
-        with pin(block):
-            data = block.read()
-            records = self.parse_block_data(data, block_offset)
-            records[local_offset][-1] = self.first_free_rec  # A positive number, which means put into free list
-            self.first_free_rec = record_offset  # update the head of free list
-            new_data = self.generate_new_data(records, block_offset)
-            block.write(new_data)
-        self.update_header()
-
-    def parse_header(self):
+    def _parse_header(self):
         """Parse the file header and return the information with a tuple"""
         block = self.buffer_manager.get_file_block(self.filename, 0)  # Get the first block
         with pin(block):
@@ -91,7 +120,7 @@ class Record:
             header_info = self.header_struct.unpack_from(data, 0)
         return header_info
 
-    def update_header(self):
+    def _update_header(self):
         """Update the file header after modifying the records"""
         block = self.buffer_manager.get_file_block(self.filename, 0)
         with pin(block):
@@ -99,17 +128,6 @@ class Record:
             header_info = (self.first_free_rec, self.rec_amount)
             data[:self.header_struct.size] = self.header_struct.pack(*header_info)
             block.write(data)
-
-            # def read(self):
-            #     block = self.buffer_manager.get_file_block(self.filename, self.block_offset)
-            #     block.pin()
-            #     data = block.read()
-            #     block.unpin()
-            #     records = [self.record_struct.unpack_from(data, offset)
-            #                for offset in range(0, len(data), self.record_struct.size)]
-            #     if len(records) <= self.record_offset:
-            #         raise RuntimeError('The record offset out of range')
-            #     return records[self.record_offset]
 
 
 class RecordManager:
@@ -134,7 +152,9 @@ class RecordManager:
     @staticmethod
     def update(filename, format, attributes, record_offset):
         record = Record(filename, format)
+        record.modify(attributes, record_offset)
 
     @staticmethod
-    def select(filename, block_offset, record_offset, format):
+    def select(filename, format, record_offset):
         record = Record(filename, format)
+        return record.read(record_offset)
--- tests/test_record_manager.py
+++ tests/test_record_manager.py
@@ -17,7 +17,10 @@ if __name__ == '__main__':
     prepare_file()
     buffer_manager = BufferManager()
     RecordManager.insert('a.rec', '<idi', (1, 3.0, 4))
-    RecordManager.delete('a.rec', '<idi', 0)
+    RecordManager.insert('a.rec', '<idi', (-1, 3.5, -1))
+    RecordManager.delete('a.rec', '<idi', 1)
+    RecordManager.insert('a.rec', '<idi', (-1, 3.0, -1))
+    print(RecordManager.select('a.rec', '<idi', 0))
     buffer_manager.flush_all()
     display_file('a.rec')
 
