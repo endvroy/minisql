b1a2896f6f2a402530f10dc996f18e7bcf343635
Roy <endvroy@gmail.com>
2016-06-02 20:36:08 +0800

    catalog manager almost done

--- catalog_manager.py
+++ catalog_manager.py
@@ -1,6 +1,8 @@
-import json
 import os
 import shutil
+from collections import OrderedDict
+import pickle
+from functools import lru_cache
 
 
 class Column:
@@ -14,82 +16,107 @@ class Column:
         return (x for x in [self.name, self.fmt, self.primary_key, self.unique])
 
 
+class Index:
+    def __init__(self, name, columns):
+        self.name = name
+        self.columns = columns
+
+
+class Table:
+    def __init__(self, name):
+        self.name = name
+        self.columns = OrderedDict()
+        self.indexes = {}
+
+    def add_column(self, column):
+        self.columns[column.name] = column
+
+    def add_index(self, index):
+        self.indexes[index.name] = index
+
+    def drop_index(self, index_name):
+        if index_name not in self.indexes:
+            raise ValueError('no index named {} on table {}'.format(index_name, self.name))
+
+        del self.indexes[index_name]
+
+
+class Metadata:
+    def __init__(self):
+        self.tables = {}
+
+    def dump(self):
+        with open('schema/metadata.pickle', 'wb') as file:
+            pickle.dump(self, file, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def add_table(self, table_name, *columns):
+        if table_name in self.tables:
+            raise ValueError('already have a table named {}'.format(table_name))
+
+        table = Table(table_name)
+        primary_keys = []
+        for column in columns:
+            table.add_column(column)
+            if column.primary_key:
+                primary_keys.append(column)
+
+        if not primary_keys:
+            raise ValueError('primary key not specified')
+
+        table.add_index(Index('PRIMARY', primary_keys))
+        self.tables[table_name] = table
+
+        # todo: create table file on disk?
+
+        self.dump()
+
+    def drop_table(self, table_name):
+        if table_name not in self.tables:
+            raise ValueError('no table named {}'.format(table_name))
+
+        shutil.rmtree('schema/tables/{}'.format(table_name))
+
+        self.dump()
+
+    def add_index(self, table_name, index_name, *columns):
+        if table_name not in self.tables:
+            raise ValueError('no table named {}'.format(table_name))
+        if not columns:
+            raise ValueError('adding index on empty columns')
+
+        self.tables[table_name].add_index(Index(index_name, columns))
+
+        table_dir = 'schema/tables/{}'.format(table_name)
+        with open(os.path.join(table_dir, '{}.index'.format(index_name)), 'wb') as file:
+            pass
+
+        self.dump()
+
+    def drop_index(self, table_name, index_name):
+        if table_name not in self.tables:
+            raise ValueError('no table named {}'.format(table_name))
+        if index_name not in self.tables[table_name].indexes:
+            raise ValueError('no index named {} on table {}'.format(index_name, table_name))
+
+        del self.tables[table_name].indexes[index_name]
+
+        os.remove('schema/tables/{table_name}/{index_name}.index'.format(table_name=table_name,
+                                                                         index_name=index_name))
+        self.dump()
+
+
 def init():
     os.makedirs('schema/tables', exist_ok=True)
 
 
+@lru_cache()
 def load_metadata():
     try:
-        with open('schema/metadata.json', 'r') as file:
-            metadata = json.load(file)
+        with open('schema/metadata.pickle', 'r') as file:
+            metadata = pickle.load(file)
             return metadata
     except FileNotFoundError:
-        metadata = {'tables': {}}
-        with open('schema/metadata.json', 'w') as file:
-            json.dump(metadata, file)
+        metadata = Metadata()
+        with open('schema/metadata.pickle', 'w') as file:
+            pickle.dump(metadata, file)
         return metadata
-
-
-def add_table(metadata, table_name, *columns):
-    if table_name in metadata['tables']:
-        raise ValueError('already have a table named {}'.format(table_name))
-
-    table_info = {
-        'columns': {},
-        'indexes': {}}
-
-    primary_keys = []
-
-    for column in columns:  # add columns
-        name, fmt, pk, unique = column
-        column_info = {
-            'format': fmt,
-            'primary_key': pk,
-            'unique': unique}
-        table_info['columns'][name] = column_info
-        if pk:
-            primary_keys.append(name)
-
-    if not primary_keys:
-        raise ValueError('primary key not specified')
-    table_info['indexes']['PRIMARY'] = primary_keys
-    metadata['tables'][table_name] = table_info
-
-    # create files
-    table_dir = 'schema/tables/{}'.format(table_name)
-    os.makedirs('table_dir', exist_ok=True)
-    with open(os.path.join(table_dir, '{}.table'.format(table_name)), 'wb') as file:
-        pass
-    with open(os.path.join(table_dir, 'PRIMARY.index'), 'wb') as file:
-        pass
-    # todo: write json back to file
-
-
-def drop_table(metadata, table_name):
-    if table_name not in metadata['tables']:
-        raise ValueError('no table named {}'.format(table_name))
-
-    shutil.rmtree('schema/tables/{}'.format(table_name))
-
-
-def add_index(metadata, table_name, index_name, *columns):
-    if table_name not in metadata['tables']:
-        raise ValueError('no table named {}'.format(table_name))
-    if not columns:
-        raise ValueError('adding index on empty columns')
-
-    metadata['tables'][table_name]['indexes'][index_name] = {'columns': columns}
-    table_dir = 'schema/tables/{}'.format(table_name)
-    with open(os.path.join(table_dir, '{}.index'.format(index_name)), 'wb') as file:
-        pass
-
-
-def drop_index(metadata, table_name, index_name):
-    if table_name not in metadata['tables']:
-        raise ValueError('no table named {}'.format(table_name))
-    if index_name not in metadata['tables'][table_name]['indexes']:
-        raise ValueError('no index named {} on table {}'.format(index_name, table_name))
-
-    del metadata['tables'][table_name]['indexes'][index_name]
-    os.remove('schema/tables/{table_name}/{index_name}.index'.format(table_name=table_name,
-                                                                     index_name=index_name))
